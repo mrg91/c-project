d172533df55e5e6b88dff4831722b89f64da394a
Mallé <269384@via.dk>
2018-04-19 11:02:37 +0200

    add function

--- MemoryProject/MemoryProject/blink.c
+++ MemoryProject/MemoryProject/blink.c
@@ -4,3 +4,16 @@
  * Created: 19/04/2018 09:20:34
  *  Author: marega
  */ 
+
+#include "blink.h"
+
+void start() {
+}
+void badAnswer() {
+}
+void win() {
+}
+void end() {
+}
+void makeLedBlinking(int* a) {
+}
--- MemoryProject/MemoryProject/game.c
+++ MemoryProject/MemoryProject/game.c
@@ -4,3 +4,13 @@
  * Created: 19/04/2018 09:21:10
  *  Author: marega
  */ 
+
+#include "game.h"
+
+
+void createSeq() {
+}
+bool compare(int* a, int* b) {
+}
+void addUserSequence() {
+}
