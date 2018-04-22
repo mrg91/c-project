a5e1d2878d7de1f8a908b40b8d0ca9c21ec444aa
Mallé <269384@via.dk>
2018-04-22 15:13:56 +0200

    game function
    createSeq
    compare
    addUserSequence

--- MemoryProject/MemoryProject/game.c
+++ MemoryProject/MemoryProject/game.c
@@ -6,17 +6,43 @@
  */ 
 
 #include "game.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
 
+int lower = 1;
+int upper = 8;
+int tracker = 0;
 void init() { 
 	DDRB = 0x00;
 	DDRA = 0xff;
 	
 }
-int createSeq(int* a, int* size) {
-	
-}
-bool compare(int* a, int* b) {
+
+
+void createSeq(int* a, int size) {
+	int i;
+	srand(time(0));
+	for(i = 0; i<size; i++)  
+       a[i] = rand()%upper + lower;
 }
 
-void addUserSequence() {
+bool compare(int* Seq, int* UserSeq, int size) { //, int BtnUser) {
+	int BtnUsr
+	while (PINB == 0xff) {
+		 BtnUsr = 0;
+	}
+	BtnUsr = PINB;
+	if (Seq[tracker] == BtnUsr) {
+		addUserSequence(UserSeq,BtnUsr, size);
+		return true;
+	}
+	return false;
+}
+
+void addUserSequence(int* tab, int value, int size) {
+	if (tracker < size) {
+		tab[tracker] = value;
+		tracker++;
+		}
 }
