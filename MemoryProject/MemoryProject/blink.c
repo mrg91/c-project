16038867e9a8c82e7f8e95bd8e12b8ee93e4c788
Kornelius Pasztor <korn_20000@hotmail.com>
2018-04-24 10:44:06 +0200

    Corrected start

--- MemoryProject/MemoryProject/blink.c
+++ MemoryProject/MemoryProject/blink.c
@@ -16,15 +16,19 @@
 
 void start() {
 	
-	DDRA = 0xFF; //makes PORTC as Output
+	DDRA = 0xFF; //makes PORTA as Output
+	int startGame = 0;
 
 while(1) //infinite loop
 {
-	
-	PORTA = 0xFF; //Turns ON All LEDs
-	_delay_ms(500); //½ second delay
-	PORTA = 0x00; //Turns OFF All LEDs
-	_delay_ms(500); //½ second delay
+	for(int i = 0; i < 3; i++)
+	{
+		PORTA = 0xFF; //Turns ON All LEDs
+		_delay_ms(500); //½ second delay
+		PORTA = 0x00; //Turns OFF All LEDs
+		_delay_ms(500); //½ second delay
+		startGame++;
+	}
 	
 	PORTA = 0xFF;
 	return 0;
